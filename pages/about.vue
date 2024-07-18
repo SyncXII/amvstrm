b3b89267152642f571ba820bd3587c018a6f1d65
Sync <SyncXII@users.noreply.github.com>
2024-07-18 23:27:11 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <script setup>
 useSeoMeta({
   ogTitle: "About",
-  ogDescription: "About amvstrm...",
+  ogDescription: "AniSync – Your Ultimate Anime Streaming Experience!",
   ogImage: "logo.png",
   ogUrl: "[og:url]",
   twitterTitle: "About",
-  twitterDescription: "About amvstrm...",
+  twitterDescription: "AniSync – Your Ultimate Anime Streaming Experience!",
   twitterImage: "logo.png",
   twitterCard: "summary",
 });
@@ -21,10 +21,7 @@ useHead({
   <v-container>
     <div class="pa-5" style="background-color: #212121">
       <h1><img src="/logo.png" alt="loho" height="50" /></h1>
-      <p>
-        amvstrm is an open source anime streaming platform that offer free anime
-        with no ADs and install it as Android or IOS web app. <br />
-        Build with Nuxt 3, Vuetify and amvstrm's V2 API.
+      <p>Welcome to AniSync – Your Ultimate Anime Streaming Experience! Dive into the world of anime like never before with AniSync, the premier destination for anime fans of all tastes and preferences. AniSync offers an unparalleled streaming experience tailored just for you.
       </p>
       <div class="mt-2">
         <v-btn
