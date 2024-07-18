757d6dd7dd3c57ac5a9244771143f968f7eeea4c
Sync <SyncXII@users.noreply.github.com>
2024-07-18 23:23:08 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <script setup>
 useSeoMeta({
   ogTitle: "Home",
-  ogDescription: "amvstrm - A streaming service for weebo...",
+  ogDescription: "AniSync - Your Ultimate Anime Streaming Experience!",
   ogUrl: useRoute().fullPath,
   twitterTitle: "Home",
-  twitterDescription: "amvstrm - A streaming service for weebo...",
+  twitterDescription: "AniSync - Your Ultimate Anime Streaming Experience!",
   twitterCard: "summary",
 });
 
