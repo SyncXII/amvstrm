8c97c94e6e1adc48193ace7e161eccb8021505fb
Sync <SyncXII@users.noreply.github.com>
2024-07-19 23:38:19 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -23,8 +23,43 @@ useHead({
           type: 'text/javascript',
           src: '//pl23817253.highrevenuenetwork.com/17/ee/0f/17ee0f5da3870577c2ae549585183a25.js',
           'data-cfasync': 'false'
-        }
-    ]
+        },
+        {
+      type: 'text/javascript',
+      charset: 'utf-8',
+      innerHTML: `
+        (function() {
+          var adBlockDetected = false;
+
+          function checkAdBlock() {
+            var adBlockTester = document.createElement('div');
+            adBlockTester.innerHTML = '&nbsp;';
+            adBlockTester.className = 'adsbox';
+            document.body.appendChild(adBlockTester);
+            window.setTimeout(function() {
+              if (adBlockTester.offsetHeight === 0) {
+                adBlockDetected = true;
+              }
+              adBlockTester.remove();
+              if (adBlockDetected) {
+                // Block access or redirect user
+                document.body.innerHTML = '<h1>Access Denied</h1><p>Please disable your ad blocker to access this site.</p>';
+              }
+            }, 100);
+          }
+
+          if (window.addEventListener) {
+            window.addEventListener('load', checkAdBlock, false);
+          } else if (window.attachEvent) {
+            window.attachEvent('onload', checkAdBlock);
+          }
+        })();
+      `,
+    },
+  ],
+  __dangerouslyDisableSanitizersByTagID: {
+    'adblock-detection-script': ['innerHTML']
+  }
 });
 </script>
 
