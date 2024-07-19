9d3b2349c5e625f375a91cc7bd17f593a68ba184
Sync <SyncXII@users.noreply.github.com>
2024-07-19 15:08:37 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -23,14 +23,14 @@ useHead({
     </div>
 </template>
 
-<script>
+<script setup>
     export default {
         data () {
             return {};
         },
         ready() {
              let scriptEl = document.createElement('script');
-             scriptEl.setAttribute('src', '//pl23816720.highrevenuenetwork.com/91/70/16/917016720b58b2739ee958d20af2d0e8.js');
+             scriptEl.setAttribute('src', 'https://pl23816720.highrevenuenetwork.com/91/70/16/917016720b58b2739ee958d20af2d0e8.js');
              scriptEl.setAttribute('data-some-param', 'paramvalue');
              this.$els.scriptHolder.appendChild(scriptEl);
         },
