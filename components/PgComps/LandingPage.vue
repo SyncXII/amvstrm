445f32147e920695e8fcf1a1f405ea46ad7e75b3
Sync <SyncXII@users.noreply.github.com>
2024-07-22 11:36:59 +0800

    Update LandingPage.vue

--- components/PgComps/LandingPage.vue
+++ components/PgComps/LandingPage.vue
@@ -200,20 +200,22 @@ const {
   </v-container>
   <!-- DESKTOP DEVICE -->
   <!-- Ad Container -->
-    <v-container class="common-container">
+  <v-container>
+    <v-row>
       <v-col cols="12">
-      <v-card>
-        <div class="pa-5" style="background-color: #212121">
-          <div id="ad-container" class="d-flex justify-center align-center pa-2" style="width: 100%; box-sizing: border-box;">
-            <div id="frame" style="max-width: 728px; width: 100%;">
+        <v-card>
+          <div class="pa-5" style="background-color: #212121;">
+            <div id="ad-container" class="d-flex justify-center align-center pa-2" style="max-width: 800px; width: 100%; box-sizing: border-box;">
+              <div id="frame" style="width: 100%; max-width: 728px;">
                 <iframe data-aa="2340453" src="//ad.a-ads.com/2340453?size=728x90&background_color=000000" style="width: 100%; height: 90px; border: 0; padding: 0; overflow: hidden; background-color: transparent;"></iframe>
                 <a id="frame-link" class="d-block text-right" style="font-size: 12px;" href="https://aads.com/?partner=2340453">Advertise here</a>
+              </div>
             </div>
           </div>
-        </div>
-      </v-card>
-    </v-col>
-    </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
   
   <v-container class="d-lg-block d-sm-none d-none" fluid>
 
