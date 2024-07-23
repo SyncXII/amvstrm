f264800deb9d045697ac3cb6ea6914c6b8bf6821
Sync <SyncXII@users.noreply.github.com>
2024-07-23 10:12:35 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -280,7 +280,7 @@ export default defineNuxtConfig({
       version: process.env.VERSION || "v2",
       posthogPublicKey: process.env.POSTHOG_PK || "phc_YtiSVvEpiFQia7T2e7UXVxCo2EqiinmnzrDlDFdxIwq",
       posthogHost: process.env.POSTHOG_HOST || "https://app.posthog.com",
-      disqus_id: process.env.DISQUS_ID || "7624313",
+      disqus_id: process.env.DISQUS_ID || "anisync-your-ultimate-anime-streaming-experience",
     },
   },
 
